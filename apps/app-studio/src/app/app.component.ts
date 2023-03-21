edea379e048dfada65a89397fa34b4c5193c55f3
Nagarajan Sivaraman <naga4042@gmail.com>
2023-03-21 20:12:39 +0530

    refactoring

--- apps/app-studio/src/app/app.component.ts
+++ apps/app-studio/src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
   selector: 'rep-studio-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
